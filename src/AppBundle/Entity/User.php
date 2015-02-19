90b18259a0d487c554d99d787eabc4584a181dcf
Katarzyna Dziadek <katarzyna.dziadek@wp.pl>
2015-02-19 23:49:09 +0100

    Aktuazliacja security

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,7 +1,7 @@
 <?php
 // src/Acme/UserBundle/Entity/User.php
 
-namespace Acme\UserBundle\Entity;
+namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
