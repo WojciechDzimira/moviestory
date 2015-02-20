955bed312fc211a1fa4a933f75539ba6c4199dbe
Katarzyna Dziadek <katarzyna.dziadek@wp.pl>
2015-02-20 10:42:34 +0100

    Wygenerowanie entity purchases

---
+++ src/AppBundle/Entity/Purchase.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Purchase
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Purchase
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="movie_id", type="integer")
+     */
+    private $movieId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Purchase
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Purchase
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set movieId
+     *
+     * @param integer $movieId
+     * @return Purchase
+     */
+    public function setMovieId($movieId)
+    {
+        $this->movieId = $movieId;
+
+        return $this;
+    }
+
+    /**
+     * Get movieId
+     *
+     * @return integer 
+     */
+    public function getMovieId()
+    {
+        return $this->movieId;
+    }
+}
