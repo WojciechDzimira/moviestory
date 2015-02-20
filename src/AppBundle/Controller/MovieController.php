765670ce62492863ac05e3c938cdd03405cdea6d
Wojciech Dzimira <s173358@v-ie.uek.krakow.pl>
2015-02-20 11:51:12 +0100

    Poprawki do v-ie

--- src/AppBundle/Controller/MovieController.php
+++ src/AppBundle/Controller/MovieController.php
@@ -53,7 +53,7 @@ class MovieController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('movie_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('movie'));
         }
 
         return array(
