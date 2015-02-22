d0fc3192a1a41197461b8377b802bc2d73228595
WojciechDzimira <wojciech.dzimira@gmail.com>
2015-02-22 18:16:45 +0100

    Update Movie.php

--- src/AppBundle/Entity/Movie.php
+++ src/AppBundle/Entity/Movie.php
@@ -1,124 +1,107 @@
-+<?php
-+
-+namespace AppBundle\Entity;
-+
-+use Doctrine\ORM\Mapping as ORM;
-+
-+/**
-+ * Movie
-+ *
-+ * @ORM\Table()
-+ * @ORM\Entity
-+ */
-+class Movie
-+{
-+    /**
-+     * @var integer
-+     *
-+     * @ORM\Column(name="id", type="integer")
-+     * @ORM\Id
-+     * @ORM\GeneratedValue(strategy="AUTO")
-+     */
-+    private $id;
-+
-+    /**
-+     * @var string
-+     *
-+     * @ORM\Column(name="title", type="string", length=255)
-+     */
-+    private $title;
-+
-+    /**
-+     * @var string
-+     *
-+     * @ORM\Column(name="info", type="text")
-+     */
-+    private $info;
-+
-+    /**
-+     * @var string
-+     *
-+     * @ORM\Column(name="price", type="decimal")
-+     */
-+    private $price;
-+
-+
-+    /**
-+     * Get id
-+     *
-+     * @return integer 
-+     */
-+    public function getId()
-+    {
-+        return $this->id;
-+    }
-+
-+    /**
-+     * Set title
-+     *
-+     * @param string $title
-+     * @return Movie
-+     */
-+    public function setTitle($title)
-+    {
-+        $this->title = $title;
-+
-+        return $this;
-+    }
-+
-+    /**
-+     * Get title
-+     *
-+     * @return string 
-+     */
-+    public function getTitle()
-+    {
-+        return $this->title;
-+    }
-+
-+    /**
-+     * Set info
-+     *
-+     * @param string $info
-+     * @return Movie
-+     */
-+    public function setInfo($info)
-+    {
-+        $this->info = $info;
-+
-+        return $this;
-+    }
-+
-+    /**
-+     * Get info
-+     *
-+     * @return string 
-+     */
-+    public function getInfo()
-+    {
-+        return $this->info;
-+    }
-+
-+    /**
-+     * Set price
-+     *
-+     * @param string $price
-+     * @return Movie
-+     */
-+    public function setPrice($price)
-+    {
-+        $this->price = $price;
-+
-+        return $this;
-+    }
-+
-+    /**
-+     * Get price
-+     *
-+     * @return string 
-+     */
-+    public function getPrice()
-+    {
-+        return $this->price;
-+    }
-+}
+<?php
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * Movie
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Movie
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="info", type="text")
+     */
+    private $info;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal")
+     */
+    private $price;
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Movie
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    /**
+     * Set info
+     *
+     * @param string $info
+     * @return Movie
+     */
+    public function setInfo($info)
+    {
+        $this->info = $info;
+        return $this;
+    }
+    /**
+     * Get info
+     *
+     * @return string 
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Movie
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
